2378ca376fb4f524eed40a6aa341e9487b502589
Idris Tong <idtong@microsoft.com>
2015-12-01 08:12:46 +0800

    refactor code and add wind

--- README.md
+++ README.md
@@ -1,2 +1,38 @@
 # vsdir
 batch add files to visual studio projects while preserving the directory structure.
+
+# motivation
+Microsoft Visual Studio is a good tool to study existing code because:
+
+* syntax parser with full language semantics
+* easily find inside entire solution
+* ...
+
+However, It's hard to add many source files to visual studio project while keep their existing directory structure. vsdir is the tool to help this.
+
+# usage
+``` Batchfile
+vsdir.exe [options] projectfile destdir
+```
+You can specify the "filter-prefix" used as common folder for all added items.
+The item path can also based on another variable, other than project directory, to make the generated project file sharable between peoples.
+Type `vsdir.exe --help` for more information.
+
+usage examples:
+
+This will add all files inside `C:\src\mona` to `mona.vcxproj`, preserving the folder structure.
+
+``` Batchfile
+vsdir "C:\src\mona.vcxproj" C:\src\mona
+```
+
+This will add all files inside `Z:\src\security\vault\VaultEngine` to `VaultEngine.vcxproj`, item paths in project file will like `$(TsSrc)\security\vault\VaultEngine\...`. The folder structure inside project are the same as they are in `Z:\src\security\vault\VaultEngine` folder.
+
+``` Batchfile
+vsdir "C:\study\vault\VaultEngine\VaultEngine.vcxproj" Z:\src\security\vault\VaultEngine --folding-var=TsSrc:Z:\src --filter-prefix=*
+```
+
+# TODO
+* option to convert existing items to SourceRoot format
+* split 'nicerGetoptPrinter' to wind
+
--- source/app.d
+++ source/app.d
@@ -5,6 +5,9 @@ import std.file;
 import std.path;
 import std.xml;
 import std.uuid;
+import std.process;
+import std.exception;
+import wind.string;
 
 /**
     Although it's called "filter" in visual studio, the same file cannot belong to different filters.
@@ -34,6 +37,48 @@ struct VisualStudioItem
     string type;
     string filter;
 };
+
+void nicerGetoptPrinter(string text, Option[] opt)
+{
+    import std.stdio : stdout;
+    nicerGetoptFormatter(stdout.lockingTextWriter(), text, opt);
+}
+
+void nicerGetoptFormatter(Output)(Output output, string text, Option[] opt)
+{
+    import std.format : formattedWrite; 
+    import std.algorithm : min, max; 
+
+    output.formattedWrite("%s\n", text); 
+
+    size_t ls, ll; 
+    bool hasRequired = false; 
+    foreach (it; opt) 
+    { 
+        ls = max(ls, it.optShort.length); 
+        ll = max(ll, it.optLong.length); 
+
+        hasRequired = hasRequired || it.required; 
+    } 
+
+    string re = " Required: ";
+
+    size_t prefixLength = ls + 1 + ll + (hasRequired? re.length : 1);
+
+    foreach (it; opt) 
+    {
+        string[] lines = it.help.split("\n");
+        foreach(i,l; lines)
+        {
+            if (i == 0)
+                output.formattedWrite("%*s %*s%*s%s\n", ls, it.optShort, ll, it.optLong, 
+                                      hasRequired ? re.length : 1, it.required ? re : " ", l);
+            else
+                output.formattedWrite("%*s%s\n", prefixLength, " ", l);
+        }
+    } 
+   
+}
 
 string GetVisualStudioDefaultTypeByFileExtension(string ext)
 {
@@ -161,49 +206,91 @@ void AddFilesToVisualStudioProject(VisualStudioItem[] items, string filename, bo
     }
 }
 
-void main(string[] argv)
+bool hasSameRoot(string dir1, string dir2)
 {
-    bool allowSpecial = false;
-    auto helpInformation = getopt(
-                                  argv, 
-                                  "root|R", "Set root path", &rootDir,
-                                  "test", "Test mode: output result contents to console", &testMode,
-                                  "allow-special-entries", "Force adding special entries: file/dir starts with '.'", &allowSpecial
+    return relativePath(dir1, dir2) != dir1;
+}
+
+bool isSubDir(string dir1, string dir2)
+{
+    string str = relativePath(dir1, dir2);
+    return str != dir1 && !str.startsWith("..");
+}
+
+string helpmsg = r"
+vsdir will scan files under destdir recursively and add them to vs project
+with the directory structure up to the project dir.
+
+If filter prefix is given, the directory structure in vs project will be changed
+to: FilterPrefix\destdir\subdir1\subdri2\...
+
+If the folding environment variable is given, item path will no longer be
+relative to the project file path. Instead, it will become an absolute path.
+The path will fold by the path that the folding var points to, e.g.:
+    $(SourceRoot)\filter\prefix\sub\dir\item.cpp
+";
+
+void main(string[] argv){
+    string filterPrefix;
+    string foldspec;
+    auto helpInformation = getopt(argv, 
+                                  "filter-prefix|P", "Set the common filer prefix for all added items", &filterPrefix,
+                                  "folding-var", "Set the folding environment variable: var[:val]", &foldspec,
+                                  "test", "Test mode: output result contents to console", &testMode
                                   );
     if (helpInformation.helpWanted || argv.length != 3)
     {
-        defaultGetoptPrinter("usage: vsdir.exe [options] vcproj dir", helpInformation.options);
+        nicerGetoptPrinter("usage: vsdir.exe [options] projectfile destdir", helpInformation.options);
+        write(helpmsg);
         return;
     }
-
-    string vcprojFileName = argv[1].absolutePath();
+   
+    string projectFileName = argv[1].absolutePath();
     string destDir = argv[2].absolutePath();
-    string vcprojFilterFileName = vcprojFileName ~ ".filters";
-    string vcprojDirName = dirName(vcprojFileName);
+    string foldVar = splitHead(foldspec, ':');
+    string foldDir = splitTail(foldspec, ':', "");
+    string projectFilterFileName = projectFileName ~ ".filters";
+    string projectDir = dirName(projectFileName);
 
-    if (rootDir.length == 0)
+    if (foldVar && foldDir.length == 0)
     {
-        writeln("root dir not given. using the same dir as vcproj");
-        rootDir = vcprojDirName;
-    }
-
-    writef(
-           "vcproj:         %s\n"
-           "vcproj filters: %s\n"
-           "root dir:       %s\n"
-           "dest dir:       %s\n",
-           vcprojFileName, vcprojFilterFileName, rootDir, destDir
-             );
-
-    // check if the root dir is really the parent of destDir
-    {
-        string filter = relativePath(destDir, vcprojDirName);
-        if (filter == destDir || filter.length >= 2 && filter[0..2] == "..")
+        foldDir = environment.get(foldVar);
+        if (foldDir.length == 0)
         {
-            writeln("error. please make sure root dir is the ancestor of destDir");
+            writeln("cannot get folding-var value from process environment.");
+            return;
+        }
+        foldDir = foldDir.absolutePath();
+        if (!hasSameRoot(destDir, foldDir))
+        {
+            writeln("folding dir and dest dir must have the same root.");
             return;
         }
     }
+
+    if (!filterPrefix)
+    {
+        if (!isSubDir(destDir, projectDir))
+        {
+            writeln("the project dir is not a parent of dest dir, please specify the fiter prefix explicitly.");
+            return;
+        }
+        filterPrefix = relativePath(destDir, projectDir);
+    }
+
+    if (filterPrefix == "*")
+        filterPrefix = "";
+
+    if (filterPrefix.length > 0 && !filterPrefix.endsWith('\\'))
+        filterPrefix ~= '\\';
+
+    writeln("project:        ", projectFileName);
+    writeln("project filters:", projectFilterFileName);
+    writeln("fold variable:  ", foldVar);
+    writeln("fold directory: ", foldDir);
+    writeln("dest dir:       ", destDir);
+    writeln("filter prefix   ", filterPrefix);
+
 
     VisualStudioItem[] items;
     // get all files
@@ -212,27 +299,23 @@ void main(string[] argv)
     {
         if (isFile(f))
         {
-            if (!allowSpecial)
+            string path = relativePath(f, destDir);
+            if (path[0] == '.')
             {
-                string path = relativePath(f, destDir);
-                if (path[0] == '.' || path.indexOf("\\.") != -1)
-                {
-                    continue;
-                }
+                continue;
             }
 
             VisualStudioItem item;
             item.absolutePath = f;
-            item.relativePath = relativePath(f, vcprojDirName);
+            item.relativePath = foldVar?
+                "$(%s)\\%s".format(foldVar, relativePath(f, foldDir)):
+                relativePath(f, projectDir);
             item.type = GetVisualStudioDefaultTypeByFileExtension(extension(f));
-            item.filter = dirName(relativePath(f, rootDir));
+            item.filter = filterPrefix ~ dirName(relativePath(f, destDir));
             items ~= item;
         }
     }
 
-    writeln(items);
-
-    AddFilesToVisualStudioProject(items, vcprojFileName, false);
-    AddFilesToVisualStudioProject(items, vcprojFilterFileName, true);
-
+    AddFilesToVisualStudioProject(items, projectFileName, false);
+    AddFilesToVisualStudioProject(items, projectFilterFileName, true);
 }
